8785b153f8ad5ad9b54ebaa150d2e8df5ad7b35c
nicolasmendez001 <mnikolas001@hotmail.com>
2020-09-18 19:43:23 -0500

    enviar el menu como llega

--- app/controllers/offered.js
+++ app/controllers/offered.js
@@ -5,6 +5,8 @@ const mesas = [new Mesa(0), new Mesa(1), new Mesa(2), new Mesa(3), new Mesa(4), 
 new Mesa(6, new Mesa(7), new Mesa(8), new Mesa(9), new Mesa(10), new Mesa(11), new Mesa(12),
   new Mesa(13), new Mesa(14))];
 
+  
+
 /*********************
  * Private functions *
  *********************/
@@ -15,8 +17,8 @@ const getData = () => new Promise((resolve, reject) => {
   //reject(error) // Esto solo se utiliza para cuando la promesa retorna un error. Es por si lo necesitan 
 })
 
-const enviarPedido = (data) => {
-  axios.post("urlCocina" + "/recibirPedido", data)
+const enviarDato = (url, data) => {
+  axios.post(url, data)
     .then(res => {
       console.log(res.body);
     })
@@ -69,12 +71,9 @@ exports.postRecibirPedido = async (req, res) => {
   try {
     const data = req.body //retorna el mismo objeto enviado 
     console.log(data);
-    if (!data.idMesa) res.status(404).json({ status: 'La id de la mesa es requerida (idMesa)' })
-    if (!data.clientes) res.status(404).json({ status: 'La lista de clientes con su pedido es requerida' })
-    if (data.clientes.length == 0) res.status(404).json({ status: 'La lista de clientes no puede estar vacia' })
-    if (!data.codFactura) res.status(404).json({ status: 'EL codigo de la factura es requerido (codFactura)' })
-    if (!data.valorTotal) res.status(404).json({ status: 'el valor total del pedido es requerido (valorTotal)' })
+
     //enviarPedido(data)
+    enviarDato("falta url", data);
     res.status(200).json({ status: 'Pedido recibido' })
   } catch (error) {
     utils.handleError(res, error)
--- app/routes/offered.js
+++ app/routes/offered.js
@@ -2,6 +2,7 @@ const express = require('express')
 const controller = require('../controllers/offered')
 const router = express.Router()
 
+
 router.get(
   '/cualquirRutacualquirRuta',
   controller.cualquierRuta
